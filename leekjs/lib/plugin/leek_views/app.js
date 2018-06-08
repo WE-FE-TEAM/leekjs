eb20d6aab03ef5897ff9e463ade4d2b83d1f2f7f
Jess <maochengjie1988@gmail.com>
2018-06-08 19:44:16 +0800

    增加 模板渲染的 plugin

---
+++ leekjs/lib/plugin/leek_views/app.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * Created by Jess on 2018/6/8.
+ */
+
+'use strict';
+
+
+
+module.exports = function(leekApp){
+
+
+
+};
---
+++ leekjs/lib/plugin/leek_views/lib/context.js
@@ -0,0 +1,17 @@
+/**
+ * 扩展 Context 对象，增加 render 方法
+ * Created by Jess on 2018/6/8.
+ */
+
+'use strict';
+
+
+
+module.exports = {
+
+    async render(tplName, data){
+
+    }
+};
+
+
